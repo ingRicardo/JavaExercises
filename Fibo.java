cc4ae9b21911c8d3d5ce2d2c5ce3fecd39cb8572
Ricardo Alfredo Macias Olvera <ra_mo116@hotmail.com>
2019-04-27 17:29:53 -0700

    Add files via upload

--- Fibo.java
+++ Fibo.java
@@ -19,7 +19,7 @@ public class Fibo {
 			System.out.println("-------->>");
 			
 			int [] vec =  new int[15];
-			for (int i = 0 ; i < 15; i++) {
+			for (int i = 0 ; i < vec.length; i++) {
 				
 				if (i<2) {
 					vec[i] = i;
