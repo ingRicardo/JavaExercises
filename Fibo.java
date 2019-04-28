08ab4d9826990b32858530a70cf12feec2d99c24
Ricardo Alfredo Macias Olvera <ra_mo116@hotmail.com>
2019-04-28 07:09:36 -0700

    normal and recursivity

--- Fibo.java
+++ Fibo.java
@@ -38,6 +38,36 @@ public class Fibo {
 			}
 			
 			
+			System.out.println("------Using-Recursivity------");
+			int val = 15;
+			int [] v =  new int[val];
+			int x =0;
+			recur(0,x,v, val);
+			
+			
 	}
+	
+	
+	public static void recur(int n, int x, int [] v, int val) {
+			
+		if (n ==0 ) {
+			x = 0;
+			v[n] = x;
+		}else if(n ==1) {
+			x=1;
+			v[n] = x;
+		}else if(n < val){
+			v[n] = v[n-1]  + v[n-2]; 
+		}
+		
+		if (n < val) {
+			System.out.println( v[n]);
+			recur(n+1,x,v, val);
+			
+		}
+		
+	}
+		
+	
 	
 }
